5edecb2be3b19c1f7815f2addcef0cb951eee307
PatrickAndrada1 <patrickanthonio993@gmail.com>
2022-09-19 15:49:29 +0700

    TugasGitPertama

--- firstCgallengeGit.js
+++ firstCgallengeGit.js
@@ -1,15 +1,26 @@
-# Pat---Tugas-Day-1
-FirstGit
-
+/*
 # First Challenge Git
 ## Latar Belakang
 
 > Ceritakan sedikit tentang latar belakangmu seperti pendidikan terakhir atau pekerjaan sebelumnya
 ## Motivasi
 
+1. Ingin mengembangkan bakat di bidang teknologi digital
+2. Ingin berkarir di dunia pemrograman
+
 > Motivasi apa yang mendorongmu untuk ikut program coding bootcamp di Hacktiv8?)
 ## Ekspektasi
 
+1. Karena didukung oleh orang tua dan sanak keluarga
+2. Rasa ingin tahu
+
 > Beri tahu kami, apa yang ingin kamu dapatkan di Hacktiv8 dan apa yang ingin kamu capai setelah lulus dari sini?
+ 
+1. Ingin mendapatkan ilmu pemrograman yang luas
+2. Ingin mendapatkan lingkungan pertemanan yang mendukung
 
 > Apakah ada hal lain yang ingin disampaikan? Bila ada, kamu bebas untuk menuliskannya
+
+1. Mohon dukungan dan doa
+2. Terima kasih
+*/
